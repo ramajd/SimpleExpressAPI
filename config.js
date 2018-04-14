84f5e2bb5cdeaa77da51cd5a57f78bccfc539b5b
Reza Alizadeh Majd <r.a.majd@gmail.com>
2018-04-15 00:14:12 +0430

    config facilities added.

---
+++ config.js
@@ -0,0 +1,18 @@
+
+var extend = require('extend');
+var fs     = require('fs');
+
+var default_config = {
+    APP_NAME : 'SimpleExpressAPI',
+    WEB : {
+        PORT : 8080
+    }
+};
+
+var running_config = default_config;
+var local_config_path = __dirname + '/config.local.js';
+if (fs.existsSync(local_config_path)) {
+    extend(true, running_config, require(local_config_path));
+}
+
+module.exports = running_config;

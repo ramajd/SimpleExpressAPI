d0d834fac6a9f91f44bc95debe533dae82283a00
Reza Alizadeh Majd <reza@Rezas-MacBook-Pro-2.local>
2019-01-13 21:18:08 +0330

    update Project homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    res.render('index', {
+        title: config.APP_NAME,
+        uptime: (process.uptime() + "").toHHMMSS()
+    });
 });
 
 module.exports = router;

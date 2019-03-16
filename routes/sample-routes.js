0d540859550ddce3a4185f9c11dd9d0296b79bac
Reza Alizadeh Majd <r.a.majd@gmail.com>
2019-03-17 01:46:27 +0330

    doc strings added

--- routes/sample-routes.js
+++ routes/sample-routes.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = [
+    /**
+     * @api             {POST} /path/to/route sample route api
+     * @apiName         sample_route
+     * @apiVersion      1.0.0
+     * @apiGroup        sample
+     * @apiDescription  sample description for route
+     * 
+     * @apiParam    {String} param1      enum of `['mobile', 'email', 'mobile_new', 'email_new']`
+     * @apiParam    {String} [param2]   users mobile no.
+     * @apiParam    {String} [param3]    users email address
+     * 
+     */
     //{ method: 'POST', path: '/path/to/route', func: function (req, res) { /* handler method */ }, permissions: [] },
 ];

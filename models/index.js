aa2f7b77ce415787dae7ca6f48f34aad4c947dd4
Reza Alizadeh Majd <r.a.majd@gmail.com>
2019-03-23 23:57:49 +0430

    auto loader added for models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
 
-exports.initialize = function (cb) {
+let models = require('require.all')({
+    dir: '.',
+    match: /-model\.js$/i,
+    recursive: false
+});
+
+models.initialize = function(cb) { 
     return cb(null);
 };
+
+module.exports = models;
